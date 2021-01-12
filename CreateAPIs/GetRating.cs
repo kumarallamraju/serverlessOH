6137e7c311da055d299eb4c253e1fc3908622a89
Kumar Allamraju <kumar.allamraju@gmail.com>
2021-01-12 10:50:31 -0800

    small change

--- CreateAPIs/GetRating.cs
+++ CreateAPIs/GetRating.cs
@@ -4,6 +4,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+//comment
 
 namespace CreateAPIs
 {
