b91133d02a0be1ade725db38bd2e94d43256b5ed
Kumar Allamraju <kumar.allamraju@gmail.com>
2021-01-14 16:09:35 -0800

    commit yaml file

---
+++ ch#3/CreateAPIs/GetProduct.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace CreateAPIs
+{
+    public static class GetProduct
+    {
+        [FunctionName("GetProduct")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "null")] HttpRequest req,
+            [CosmosDB("serverlessDB", "products", ConnectionStringSetting = "RatingsDatabase",
+            SqlQuery = "SELECT * from c where c.productId = {productId}")] IEnumerable<RatingModel> product,
+            ILogger log)
+        {
+            log.LogInformation("Getting Product");
+            if (product == null)
+            {
+                return new NotFoundResult();
+            }
+            else
+            {
+                return new OkObjectResult(product);
+            }
+        }
+    }
+}
+
+
+
